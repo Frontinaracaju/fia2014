bd0afbc651585afe38995eda1595d04259da619d
Jefersson Nathan <admin@phpse.net>
2014-04-09 10:30:38 -0300

    Separação dos arquivos que são inclusos - Fix #1
    - Criação de uma pasta `includes` para colocar arquivos de layout
    - date_timezone_set() e os cabeçalhos http são enviados da index
    - `index.php` como front controller

--- index.php
+++ index.php
@@ -1,116 +1,12 @@
-<?php include '_doctype.php'; ?>
-  <head>
-      <title>Rock and Code - Front in Aracaju 2014</title>
-       <?php include '_head.php'; ?>
-    	 <!-- Styles -->
-       <?php include '_scripts_topo.php'; ?>
-  </head>
-  <body>
-    <?php include '_chromeframe.php'; ?>
-    <div class="main">
-      <section class="wrapper1">
-        <div class="redes-topo">
-          <ul>
-            <li><a href="https://twitter.com/frontinaracaju" target="_blank"><img src="assets/img/redes-twitter.png" alt="Ícone de acesso ao Twitter"></a></li>
-            <li><a href="https://www.facebook.com/frontinaracaju" target="_blank"><img src="assets/img/redes-face.png" alt="Ícone de acesso ao Facebook"></a></li>
-            <li><a href="mailto:contato@frontinaracaju.com.br" target="_blank"><img src="assets/img/redes-email.png" alt="Ícone de acesso webmail"></a></li>
-          </ul>
-        </div>
-        <div class="container-fluid">
-          <div class="container relative">
-            <nav>
-              <div class="btn-palestrar"><a class="btnpadrao">Quero Palestrar</a></div>
-              <div class="logofia"><h1 class="logo">Fia</h1></div>
-              <div class="btn-patrocinar"><button class="btnpadrao">Quero Patrocinar</button></div>
-            </nav>
-            <div class="selofia"><img src="assets/img/selo.png" alt="Selo Rock FIA 2014"></div>
-            <div class="slogan">
-              <h3 class="title">Um verdadeiro evento repleto</h3>
-              <h2 class="title">de código e rock n’ roll!</h2>
-            </div>
-            <div class="date">
-              <div class="h2">02 de  Agosto 2014 -  AABB</div>
-            </div>
-          </div>
-        </div>  
-      </section>
-      <?php include("video.php") ?>
-      <section class="wrapper3">
-        <div class="container relative">
-          <div class="imggaleria"></div>
-          <div class="escudo"></div>
-          <div class="camera"></div>
-          <div class="frasegaleria">
-            <h2 class="title">Confira as fotos da edição 2013</h2>
-          </div>
-          <div class="fotogaleria">
-            <div class="g-left">
-              <div class="gfotos">
-                <a href="assets/img/fotos/DSCN6240.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6240.jpg&largura=111&altura=111&qualidade=100" class="left"></a>
-                <a href="assets/img/fotos/DSCN6235.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6235.jpg&largura=111&altura=111&qualidade=100" class="right"/></a>
-              </div>
-              <div class="gfotos">
-                <a href="assets/img/fotos/DSCN6307.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6307.jpg&largura=239&altura=239&qualidade=100" alt=""></a>
-              </div>  
-            </div>
-            <div class="g-right">
-              <div class="gfotos">
-                <a href="assets/img/fotos/DSCN6337.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6337.jpg&largura=111&altura=111&qualidade=100" alt="" class="left"></a>
-                <a href="assets/img/fotos/DSCN6713.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6713.jpg&largura=111&altura=111&qualidade=100" alt="" class="right"></a>
-              </div>
-              <div class="gfotos">
-                <a href="assets/img/fotos/DSCN6735.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6735.jpg&largura=111&altura=111&qualidade=100" alt="" class="left"></a>
-                <a href="assets/img/fotos/DSCN6911.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6911.jpg&largura=111&altura=111&qualidade=100" alt="" class="right"></a>
-              </div>
-              <div class="gfotos">
-                <a href="assets/img/fotos/DSCN6930.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6930.jpg&largura=111&altura=111&qualidade=100" alt="" class="left"></a>
-                <a href="assets/img/fotos/DSCN6458.jpg" rel="shadowbox"><img src="inc_thumb3.php?arquivo=assets/img/fotos/DSCN6458.jpg&largura=111&altura=111&qualidade=100" alt="" class="right"></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section class="wrapper4">
-        <div class="fogolocal"></div>
-        <div class="fogolocal2"></div>
-        <h2 class="title">Um evento de front-end  para o mar em Aracaju</h2>
-        <div class="container-fluid fotolocal"></div>
-        <div class="raio1"></div>
-        <div class="raio2"></div>
-        <div class="raio3"></div>
-        <div class="raio4"></div>
-        <div class="bg-frase-local">
-          <h3>Front-End + Rock = Isso vai pegar fogo!</h3>
-        </div>
-      </section>
-      <section class="wrapper5 relative">
-        <h2 class="title quemfaz">Quem faz o evento</h2>
-        <div class="firefooter"></div>
-        <div class="italo">
-          <h3>Ítalo Waxman</h3>
-          <p>Designer / Front-end</p>
-          <ul class="redesf">
-            <li class="twitter"><a href="https://twitter.com/italowaxman"></a></li>
-            <li class="facebook"><a href="https://www.facebook.com/italowaxman?fref=ts"></a></li>
-            <li class="email"><a href="mailto:italowaxman@gmail.com"></a></li>
-          </ul>
-        </div>
-        <div class="julio">
-          <h3>Julio Cezar</h3>
-          <p>Front-end Developer</p>
-          <ul class="redesf2">
-            <li class="twitter"><a href="https://twitter.com/cezinhajc"></a></li>
-            <li class="facebook"><a href="https://www.facebook.com/cezinhajc"></a></li>
-            <li class="email"><a href="mailto:cezinhajc@gmail.com"></a></li>
-          </ul>
-        </div>
-        <div class="fotoorg"></div>
-        <div class="raiof1"></div>
-        <div class="raiof2"></div>
-        <div class="logofiafooter"></div>
-      </section>
-    </div>
-		<?php  include '_js_scripts.php'; ?>
-    <?php include '_analytics.php'; ?>
-  </body>
-</html>
+<?php
+date_timezone_set('America\Sao_Paulo');
+
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+header('Content-Type: text/html; charset=utf-8');
+
+ob_start();
+require dirname(__FILE__).'/includes/main.php';
+
+$content = ob_get_contents();
